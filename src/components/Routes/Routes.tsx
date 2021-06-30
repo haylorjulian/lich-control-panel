2158007a3d27cac0f0397b85567ba8230944b259
Julian Haylor <haylorjulian@gmail.com>
2021-06-30 17:50:21 +0100

    ♻️  extract consts and cleanup component structs

--- src/components/Routes/Routes.tsx
+++ src/components/Routes/Routes.tsx
@@ -1,40 +1,20 @@
-import { Switch, Route, Link } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 import Dashboard from "../Dashboard/Dashboard";
 
 import { useFetchInstances } from "../../api/instances";
-
-import styles from "./Routes.module.scss";
+import { linkToCMC, linkToDashboard } from "./paths";
 
 export function Routes() {
   const { data: instances, isFetched } = useFetchInstances();
   return (
-    <div className={styles.routewrap}>
-      <div className={styles.header}>
-        <p className={styles.title}>Lich Dashboard</p>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/dashboard">Dashboard</Link>
-            </li>
-            <li>
-              <Link to="/cmc-trends">CMC Trends</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-
-      <Switch>
-        <Route path="/cmc-trends">
-          <h1>CMC Trends</h1>
-        </Route>
-        <Route path="/dashboard">
-          {isFetched ? <Dashboard instances={instances!}></Dashboard> : null}
-        </Route>
-        <Route path="/login">
-          <h1>Login</h1>
-        </Route>
-      </Switch>
-    </div>
+    <Switch>
+      <Route path={linkToCMC}>
+        <h1>CMC Trends</h1>
+      </Route>
+      <Route path={linkToDashboard}>
+        {isFetched ? <Dashboard instances={instances!}></Dashboard> : null}
+      </Route>
+    </Switch>
   );
 }
